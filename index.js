ad85465a7b0ce86b90486f674be90a144e36c593
Masud_Rahman22 <masudrhmn6560@gmail.com>
2023-11-08 04:09:47 +0600

    for blogs executed the post method to post blogs to mongo and then get it all by get method.

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ app.use(cors({
 }));
 app.use(cookieParser())
 
-const verifyToken = (req,res,next)=>{
+const verifyToken = (req, res, next) => {
     const token = req?.cookies?.token;
-    console.log('token here',token);
-    if(!token){
-        return res.status(401).send({message: 'unauthorized access'})
+    console.log('token here', token);
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
-        if(err){
-            return res.status(401).send({message: 'bad unauthorized access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'bad unauthorized access' })
         }
-        else{
-            req.user = decoded;
+        else {
+            req.decoded = decoded;
             next()
         }
     })
@@ -52,20 +52,20 @@ async function run() {
         const newsletterCollection = client.db("newsletters").collection("newsletter")
         const categoryCollection = client.db("categories").collection("category")
         const wishlistCollection = client.db("wishlists").collection("wishlist")
-
+        const commentsCollection = client.db("comments").collection("comment")
         // jwt authorization
 
-        app.post('/jwt',async (req,res)=>{
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log(user);
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn: '1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res
-            .cookie('token', token , {
-                httpOnly: true,
-                secure: false,
-                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
-            })
-            .send(token)
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+                })
+                .send(token)
         })
 
         // for blogs
@@ -76,7 +76,7 @@ async function run() {
             res.send(result);
         })
 
-        
+
 
         // app.get('/blogs',async(req,res)=>{
         //     const result = await blogsCollection.find(req.body).toArray()
@@ -84,14 +84,14 @@ async function run() {
         // })
 
         app.get('/blogs/sort', async (req, res) => {
-            const result = await blogsCollection.find().sort({currentDate:-1,currentTime:-1}).toArray()
+            const result = await blogsCollection.find().sort({ currentDate: -1, currentTime: -1 }).toArray()
             res.send(result)
         })
 
-        app.get('/blogs',async(req,res)=>{
+        app.get('/blogs', async (req, res) => {
             let queryObj = {};
             const category = req.query.category
-            if(category){
+            if (category) {
                 queryObj.category = category
             }
             const result = await blogsCollection.find(queryObj).toArray()
@@ -100,9 +100,9 @@ async function run() {
 
         // for blog details
 
-        app.get('/blogDetails/:id',async(req,res)=>{
+        app.get('/blogDetails/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await blogsCollection.findOne(query)
             res.send(result)
         })
@@ -123,11 +123,11 @@ async function run() {
             const updatedCards = req.body;
             const cards = {
                 $set: {
-                    img : updatedCards.img,
-                    title : updatedCards.title,
-                    category : updatedCards.category,
-                    shortDescription : updatedCards.shortDescription,
-                    longDescription : updatedCards.longDescription,
+                    img: updatedCards.img,
+                    title: updatedCards.title,
+                    category: updatedCards.category,
+                    shortDescription: updatedCards.shortDescription,
+                    longDescription: updatedCards.longDescription,
                 }
             }
             const result = await blogsCollection.updateOne(filter, cards, options);
@@ -136,19 +136,19 @@ async function run() {
 
         // blog category
 
-        app.post('/blogs/categories',async(req,res)=>{
+        app.post('/blogs/categories', async (req, res) => {
             const result = await categoryCollection.insertOne(req.body)
             res.send(result)
         })
 
-        app.get('/blogs/categories',async(req,res)=>{
+        app.get('/blogs/categories', async (req, res) => {
             const result = await categoryCollection.find().toArray()
             res.send(result)
         })
 
         // for newsletter
 
-        app.post('/newsletter',async(req,res)=>{
+        app.post('/newsletter', async (req, res) => {
             const newsletter = req.body;
             const result = await newsletterCollection.insertOne(newsletter)
             res.send(result)
@@ -156,30 +156,52 @@ async function run() {
 
         // for wishlist
 
-        app.post('/wishlist',async(req,res)=>{
+        app.post('/wishlist', async (req, res) => {
             const wishlist = req.body;
             const result = await wishlistCollection.insertOne(wishlist)
             res.send(result)
         })
 
-        app.get('/wishlist',verifyToken, async(req,res)=>{
-            console.log(req.query?.email);
-            console.log(req.user?.email);
-            if(req.query?.email !== req.user?.email){
-                return res.send({message: 'access denied'})
+        app.get('/wishlist', verifyToken, async (req, res) => {
+            const email = req.query.email
+            if (!email) {
+                res.send([])
             }
-            let query = {}
-            if(req.query?.email){
-                query = {email: req.query?.email}
+            // check valid user
+            const decodedEmail = req.decoded.email
+            console.log(decodedEmail);
+            if (email !== decodedEmail) {
+                res.status(403).send({ message: 'Forbidden Access' })
             }
+            const query = { email: email }
             const result = await wishlistCollection.find(query).toArray()
             res.send(result)
         })
-        app.delete('/wishlist/:id', async(req,res)=>{
+        app.delete('/wishlist/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await wishlistCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // for comments
+
+        app.post('/comments', async (req, res) => {
+            const result = await commentsCollection.insertOne(req.body)
+            res.send(result)
+        })
+
+        app.get('/comments', async (req, res) => {
+            const result = await commentsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/comments/:id',async(req,res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const query = {blogsId: id}
+            const result = await commentsCollection.find(query).toArray()
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
